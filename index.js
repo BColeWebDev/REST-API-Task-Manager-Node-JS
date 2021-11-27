059413e3a7d4541279b86be32aa6b59a017848f4
BColeWebDev <briancole1994@hotmail.com>
2021-11-27 12:27:45 -0500

    adjusted express middlware

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ import connectDB from "./db/connection.js"
 import app from "./server.js"
 import tasks from "./routes/tasks.js"
 import env from "dotenv"
+import notFoundMiddleware from "./middleware/not-found.js"
+import errorHandlerMiddleware from "./middleware/error-handler.js"
 env.config()
 //Middleware - Functions that handle that handle to the request and response object
 app.use(express.json())
 
 // base route
-app.get('/hello', (req, res) => res.status(200).send('<h1> Home Page </h1> '))
-
-
-
+app.use(express.static('./public'));
 app.use('/api/v1/tasks', tasks)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
+const port = process.env.PORT || 5000;
 
-// client-side routes - localhost:3000
-// server-side routes - localhost:5000 
 
 // router.get('localhost:3000/api/v1/tasks', getPeople)
 // router.post('localhost:3000/api/v1/tasks', createPerson)
@@ -24,7 +24,6 @@ app.use('/api/v1/tasks', tasks)
 // router.put('localhost:3000/api/v1/tasks/:id', updatePerson)
 // router.delete('localhost:3000/api/v1/tasks/:id', deletePerson)
 
-// listening to port 5000
 
 
 
@@ -32,7 +31,7 @@ const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
 
-        app.listen(5000, () => {
+        app.listen(port, () => {
             console.log('server is listening on port 5000...')
         })
     } catch (error) {

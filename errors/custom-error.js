0daa9ef887563ce4705e604341cfff02543e08d0
BColeWebDev <briancole1994@hotmail.com>
2021-11-27 12:27:18 -0500

    updated middlware

--- errors/custom-error.js
+++ errors/custom-error.js
@@ -13,4 +13,5 @@ const createCustomError = (msg, statusCode) => {
     return new CustomAPIError(msg, statusCode)
 }
 
+
 export default { CustomAPIError, createCustomError }
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,6 @@
-import { CustomAPIError } from '../errors/custom-error'
+import Error from '../errors/custom-error.js'
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
+    if (err instanceof Error.CustomAPIError) {
         return res.status(err.statusCode).json({ msg: err.message })
     }
     return res.status(500).json({ msg: 'Something went wrong, please try again' })
